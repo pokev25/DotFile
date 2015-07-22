e6b067f9859e8855f211880dcb7a70ad9ca1708a
pokev25 <pokev25@gmail.com>
2015-07-22 10:26:58 +0900

    yum-utils 추가 설치 패키지 정

--- install_test.sh
+++ install_test.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 
 # el, epel, remi  저장소 추가
-EPEL ='https://dl.fedoraproject.org/pub/epel/epel-release-latest-6.noarch.rpm'
-REMI ='http://rpms.remirepo.net/enterprise/remi-release-6.rpm'
+EPEL=https://dl.fedoraproject.org/pub/epel/epel-release-latest-6.noarch.rpm
+REMI=http://rpms.remirepo.net/enterprise/remi-release-6.rpm
 
-yum -y --nogpgcheck install "${EPEL}" >/dev/null
-yum -y --nogpgcheck install "${REMI}" >/dev/null
+yum -y --nogpgcheck install ${EPEL} >/dev/null
+yum -y --nogpgcheck install ${REMI} >/dev/null
 
 # mariadb, nginx 저장소 추가
 #cp ./MariaDB_10.repo /etc/yum.repos.d/MariaDB.repo
 cp ./nginx.repo /etc/yum.repos.d
 
 # 기본 설치 프로그램
+yum -y install yum-utils
 yum-config-manager --enable remi
 yum -y update
 #yum -y install MariaDB-server MariaDB-client
 #yum -y install httpd
-yum -y install nginx
+#yum -y install nginx
 yum -y install php-fpm php-mysql php-pecl-zendopcache php-xml php-mbstring php-pdo php-gd
-yum -y install fail2ban htop tmux vsftpd vim lrzsz wget lftp
-yum -y install java-1.8.0-openjdk-devel
+#yum -y install fail2ban htop vsftpd 
+yum -y install  tmux vim lrzsz wget lftp
+#yum -y install java-1.8.0-openjdk-devel
 
 . ./setting.sh
