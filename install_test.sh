4e766b3844fb76d2745e499a5e8406c3d640c660
pokev25 <pokev25@gmail.com>
2015-07-22 10:08:47 +0900

    테스트용 스크립트 수

--- install_test.sh
+++ install_test.sh
@@ -8,14 +8,15 @@ yum -y --nogpgcheck install "${EPEL}" >/dev/null
 yum -y --nogpgcheck install "${REMI}" >/dev/null
 
 # mariadb, nginx 저장소 추가
-cp ./MariaDB_10.repo /etc/yum.repos.d/MariaDB.repo
-#cp ./nginx.repo /etc/yum.repos.d
+#cp ./MariaDB_10.repo /etc/yum.repos.d/MariaDB.repo
+cp ./nginx.repo /etc/yum.repos.d
 
 # 기본 설치 프로그램
 yum-config-manager --enable remi
 yum -y update
-yum -y install MariaDB-server MariaDB-client
+#yum -y install MariaDB-server MariaDB-client
 #yum -y install httpd
+yum -y install nginx
 yum -y install php-fpm php-mysql php-pecl-zendopcache php-xml php-mbstring php-pdo php-gd
 yum -y install fail2ban htop tmux vsftpd vim lrzsz wget lftp
 yum -y install java-1.8.0-openjdk-devel
