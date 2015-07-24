8f6e39b11f1e8f89b3322501eb684870366b4da2
pokev25 <pokev25@gmail.com>
2015-07-24 20:01:37 +0900

    최종

--- ffmpeg_setup.sh
+++ ffmpeg_setup.sh
@@ -2,8 +2,8 @@
 
 rpm --import http://packages.atrpms.net/RPM-GPG-KEY.atrpms
 
-rpm -ivh http://dl.atrpms.net/all/atrpms-repo-6-7.el6.x86_64.rpm
+rpm -Uvh http://dl.atrpms.net/all/atrpms-repo-6-7.el6.x86_64.rpm
 
-yum -y --enablerepo=atrpms install ffmpeg ffmpeg-devel
+yum -y install ffmpeg ffmpeg-devel
 
 yum -y install php-ffmpeg
