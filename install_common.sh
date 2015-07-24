2129f624992d448c62eae41c06d87e834ccebe98
pokev25 <pokev25@gmail.com>
2015-07-24 20:41:58 +0900

    LAMP기본 설정 완

--- install_common.sh
+++ install_common.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# el, epel, remi  저장소 추가
+# epel, remi  저장소 추가
 EPEL=https://dl.fedoraproject.org/pub/epel/epel-release-latest-6.noarch.rpm
 REMI=http://rpms.remirepo.net/enterprise/remi-release-6.rpm
 
@@ -14,10 +14,8 @@ cp ./MariaDB_55.repo /etc/yum.repos.d/MariaDB.repo
 yum -y install yum-utils
 yum-config-manager --enable remi
 yum -y update
-yum -y httpd
-yum -y install php-cli php-fpm php-mysql php-pecl-zendopcache php-xml php-mbstring php-pdo php-gd
+yum -y install httpd
+yum -y install php php-cli php-fpm php-mysql php-pecl-zendopcache php-xml php-mbstring php-pdo php-gd
 yum -y install fail2ban htop vsftpd 
 yum -y install tmux vim lrzsz wget lftp
 yum -y install java-1.8.0-openjdk-devel
-
-
