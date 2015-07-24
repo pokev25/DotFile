cbbb142d22c19e3f894c528a84be6dd0e36174ba
pokev25 <pokev25@gmail.com>
2015-07-24 19:52:51 +0900

    ffmpeg  설치 추가

---
+++ ffmpeg_setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash 
+
+rpm --import http://packages.atrpms.net/RPM-GPG-KEY.atrpms
+
+rpm -ivh http://dl.atrpms.net/all/atrpms-repo-6-7.el6.x86_64.rpm
+
+yum -y --enablerepo=atrpms install ffmpeg ffmpeg-devel
+
+
