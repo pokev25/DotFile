4cb8861bacd39d16841ea0e5153a006140b1e473
dummy <pokev25@gmail.com>
2015-08-14 12:05:45 +0900

    spf13용 vim 설치
    spf13용 vim 설치로 변경

--- setting.sh
+++ setting.sh
@@ -6,6 +6,12 @@ chmod +x tmx
 cp tmx /usr/local/bin
 
 # vim 설정
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-cp .vimrc ~/
-vim +PluginInstall +qall
+#git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+#cp .vimrc ~/
+#vim +PluginInstall +qall
+
+# spf13용 vim 설치
+curl http://j.mp/spf13-vim3 -L -o - | sh
+
+cp .vimrc.local ~/
+cp .vimrc.before.local ~/
