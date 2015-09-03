12b2f1cbb8ced726449a28ad0ada3cc46b2b9c2a
pokev25 <pokev25@gmail.com>
2015-09-03 14:59:17 +0900

    인스톨 스크립트 apache, nginx 분리
    - install_ap : apache+ php
    - install_np : nginx-php
    - install_npm : nginx+php+MariaDB

--- install_ap.sh
+++ install_ap.sh
@@ -16,7 +16,7 @@ yum -y install yum-utils
 yum-config-manager --enable remi
 yum -y update
 yum -y install httpd
-yum -y install php php-cli php-fpm php-mysql php-pecl-zendopcache php-xml php-mbstring php-pdo php-gd
+yum -y install php php-mysql php-pecl-zendopcache php-xml php-mbstring php-pdo php-gd
 yum -y install fail2ban htop vsftpd 
 yum -y install tmux vim lrzsz wget lftp
 yum -y install java-1.8.0-openjdk-devel
