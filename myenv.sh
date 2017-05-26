ec8e6329405846cc0e015ec8b88bbecc90c78de6
pokev25 <pokev25@gmail.com>
2017-05-26 13:49:46 +0900

    set my env

--- myenv.sh
+++ myenv.sh
@@ -2,10 +2,18 @@
 
 # tmux
 cp .tmux.conf ~/
+if [$1 = 'root']; then
+  cp tmx /usr/local/bin
+  chmod +x /usr/local/bin/tmx
+fi
 
-# vim
+# vim : from vimbootstrap
+curl 'http://vim-bootstrap.com/generate.vim' --data 'langs=javascript&langs=php&langs=html&langs=ruby&langs=python&langs=perl&editor=vim' > .vimrc
 cp .vimrc ~/
 cp .vimrc.local ~/
 cp .vimrc.local.bundles ~/
 
 vim +PlugInstall +qall
+
+# fzf setup
+~/.fzf/install
