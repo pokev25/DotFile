6e935e7a6fe205841583588796b7ad6d28265863
pokev25 <pokev25@gmail.com>
2022-09-01 11:44:10 +0900

    zsh  설정

--- install_zsh.sh
+++ install_zsh.sh
@@ -9,3 +9,9 @@ if [ $EUID == 0 ]; then
 fi
 
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+
+cp .zshrc ~/
